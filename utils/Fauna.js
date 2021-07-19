3fe2d4f217e7a086b05197a9369961cf3fd0e3f3
Divine Orji <dpkreativ@gmail.com>
2021-07-19 17:22:12 +0100

    create function to get snippets from fauna db

--- utils/Fauna.js
+++ utils/Fauna.js
@@ -1,31 +1,45 @@
-const faunadb = require('faunadb');
+const faunadb = require("faunadb");
 const faunaClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
 const q = faunadb.query;
 
 const getSnippets = async () => {
-    //TODO: get snippets
+  //TODO: get snippets
+  const { data } = await faunaClient.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection("snippets"))),
+      q.Lambda("ref", q.Get(q.Var("ref")))
+    )
+  );
+
+  const snippets = data.map((snippet) => {
+    snippet.id = snippet.ref.id;
+    delete snippet.ref;
+    return snippet;
+  });
+
+  return snippets;
 };
 
 const getSnippetById = async (id) => {
-    //TODO: get snippet by id
+  //TODO: get snippet by id
 };
 
 const createSnippet = async (code, language, description, name) => {
-    //TODO: create snippet
+  //TODO: create snippet
 };
 
 const updateSnippet = async (id, code, language, name, description) => {
-    //TODO: update snippet
+  //TODO: update snippet
 };
 
 const deleteSnippet = async (id) => {
-    //TODO: delete snippet
+  //TODO: delete snippet
 };
 
 module.exports = {
-    createSnippet,
-    getSnippets,
-    getSnippetById,
-    updateSnippet,
-    deleteSnippet,
+  createSnippet,
+  getSnippets,
+  getSnippetById,
+  updateSnippet,
+  deleteSnippet,
 };
